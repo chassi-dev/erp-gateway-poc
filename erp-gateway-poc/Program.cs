55db5e81ec7094727e8481d1af12d46242770c7d
Sean Clements <sean@chassi.com>
2020-01-03 16:13:53 -0700

    set up listeners to mutliple queues

--- erp-gateway-poc/Program.cs
+++ erp-gateway-poc/Program.cs
@@ -8,15 +8,133 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Messaging;
+using System.Timers;
 
 namespace erp_gateway_poc {
     class Program {
+
+		static int messageId = 0;
+
 		const string SiteURL = "http://localhost/AcumaticaERP";
 		const string Username = "admin";
 		const string Password = "******";
 		const string Tenant = "SalesOrder";
 
+		static List<string> queueNames = new List<string> {
+			@".\private$\feelgoodqueue",
+			@".\private$\feelbadqueue"
+		};
+
 		static void Main(string[] args) {
+
+			Timer writeTimer = new Timer();
+			writeTimer.Interval = 10000; // 15 seconds
+			writeTimer.Elapsed += new ElapsedEventHandler(QueueWrite);
+			writeTimer.Start();
+
+			List<MessageQueue> queueList = ListenToQueues();
+			Console.WriteLine($"Number of Queues {queueList.Count}");
+			Console.ReadLine();
+			CloseQueues(queueList);
+			Console.ReadLine();
+		}
+
+		private static MessageQueue GetQueueReference(string queueName) {
+			MessageQueue queue;
+			if (!MessageQueue.Exists(queueName))
+				queue = MessageQueue.Create(queueName);
+			else
+				queue = new MessageQueue(queueName);
+			return queue;
+		}
+
+
+		private static List<MessageQueue> ListenToQueues() {
+
+			List<MessageQueue> queueList = new List<MessageQueue>();
+
+			foreach (string queueName in queueNames) {
+
+				MessageQueue mq = GetQueueReference(queueName);
+
+				mq.ReceiveCompleted += new ReceiveCompletedEventHandler(MyReceiveCompleted);
+
+				mq.BeginReceive();
+
+				Console.WriteLine($"Listening on queue: {queueName}");
+				queueList.Add(mq);
+			}
+
+			return queueList;
+		}
+
+		private static void CloseQueues(List<MessageQueue> queueList) {
+
+			foreach (MessageQueue mq in queueList) {
+
+				Console.WriteLine($"Closing listener for queue: {mq.QueueName}");
+
+				// Remove the event handler before closing the queue
+				mq.ReceiveCompleted -= MyReceiveCompleted;
+				mq.Close();
+				mq.Dispose();
+			}
+		}
+
+		public static void QueueWrite(Object sender, ElapsedEventArgs args) {
+			try {
+
+				MessageQueue goodQueue = GetQueueReference(@".\private$\feelgoodqueue");
+				Console.WriteLine($"Sending message for {messageId} to queue {goodQueue.QueueName}");
+				goodQueue.Send($"Message for the queue for message #{messageId}", $"Message {messageId}");
+
+				MessageQueue badQueue = GetQueueReference(@".\private$\feelbadqueue");
+				Console.WriteLine($"Sending message for {messageId} to queue {badQueue.QueueName}");
+				badQueue.Send($"Message for the queue for message #{messageId}", $"Message {messageId}");
+
+				messageId++;
+			}
+
+			catch (MessageQueueException ex) {
+
+				Console.WriteLine(ex.Message);
+
+			}
+		}
+
+		private static void StartListening() {
+
+			MessageQueue myQueue = GetQueueReference(@".\private$\feelgoodqueue");
+
+			myQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(MyReceiveCompleted);
+
+			myQueue.BeginReceive();
+		}
+
+		private static void MyReceiveCompleted(Object source, ReceiveCompletedEventArgs asyncResult) {
+
+			try {
+
+				MessageQueue mq = (MessageQueue)source;
+				mq.Formatter = new XmlMessageFormatter(new string[] { "System.String" });
+				Message message = mq.EndReceive(asyncResult.AsyncResult);
+
+				if (message != null) {
+					Console.WriteLine($"{message.Label}: {message.Body} from queue: {mq.QueueName}");
+				}
+
+				mq.BeginReceive();
+			}
+			catch (MessageQueueException ex) {
+
+				Console.WriteLine(ex.Message);
+			}
+
+			return;
+		}
+
+		private static void basicStuff() {
 			var authApi = new AuthApi(SiteURL);
 			var cookieContainer = new CookieContainer();
 			authApi.Configuration.ApiClient.RestClient.CookieContainer = cookieContainer;
@@ -51,5 +169,6 @@ namespace erp_gateway_poc {
 			//share cookie container between API clients because we use different client for authentication and interaction with endpoint
 			Configuration.Default.ApiClient.RestClient.CookieContainer = authApi.Configuration.ApiClient.RestClient.CookieContainer;
 		}
+
 	}
 }
