7ef1c27bf6bfe0acd2d0f09810faea18be84655c
Sean Clements <sean@chassi.com>
2020-01-06 16:19:54 -0700

    a little cleanup

--- erp-gateway-poc/Program.cs
+++ erp-gateway-poc/Program.cs
@@ -158,35 +158,6 @@ namespace erp_gateway_poc {
 			}
 		}
 
-		// Code from RestAPI client, just keeping it here for reference for a bit
-
-		private static void basicStuff() {
-			var authApi = new AuthApi(SiteURL);
-			var cookieContainer = new CookieContainer();
-			authApi.Configuration.ApiClient.RestClient.CookieContainer = cookieContainer;
-			try {
-				LogIn(authApi);
-
-				var soApi = new SalesOrderApi();
-				Console.WriteLine("Reading Sales Orders...");
-
-				var soOrders = soApi.SalesOrderGetList(top: 5);
-
-				foreach (var order in soOrders) {
-					Console.WriteLine("Order Type: " + order.OrderType.Value + "; Order Number: " + order.OrderNbr.Value);
-				}
-			}
-			catch (Exception e) {
-				Console.WriteLine(e.Message);
-			}
-			finally {
-				//we use logout in finally block because we need to always logut, even if the request failed for some reason
-				authApi.AuthLogout();
-				Console.WriteLine("Logged Out...");
-			}
-			Console.ReadLine();
-		}
-
 		private static void LogIn(AuthApi authApi) {
 			authApi.AuthLogin(new Credentials(Username, Password, Tenant));
 			Console.WriteLine("Logged In...");
